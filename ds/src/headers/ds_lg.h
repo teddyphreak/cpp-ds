a76ef69ef6c19adcc37af64f7002224ebe1ad26e
cookao <alejandro.cook@gmail.com>
2012-12-05 21:23:18 +0100

    up

--- ds/src/headers/ds_lg.h
+++ ds/src/headers/ds_lg.h
@@ -73,6 +73,21 @@ namespace ds_lg {
 
 	inline int64 f_mux2(int64_cpc a, int64_cpc b, int64_cpc c){ return (*a & ~(*c)) | (*b & *c);}
 
+	class Monitor {
+	public:
+		virtual void log(const int64& v);
+	};
+
+	class NodeObserver : public Monitor {
+		int64 val;
+		virtual void log(const int64& v) {
+			val = v;
+		}
+		int64 get_value() const {return val;}
+	};
+
+	typedef std::vector<Monitor*> monitor_container;
+
 	class LGNode {
 	public:
 		int level;
@@ -82,8 +97,18 @@ namespace ds_lg {
 		LGNode(const std::string &t):level(-1),gate(0),endpoint(false),type(t){}
 		void set_gate(ds_structural::Gate* g) {gate = g;}
 		ds_structural::Gate* get_gate() const{return gate;}
-		virtual void sim()=0;
-		virtual void propagate()=0;
+		virtual int64 sim()=0;
+		virtual bool propagate() {
+			int64 l = o;
+			int64 v = sim();
+			for (monitor_container::iterator it = monitors.begin();it!=monitors.end();it++){
+				Monitor *m = *it;
+				m->log(o);
+			}
+			if (!endpoint)
+				return l!=v;
+			return false;
+		}
 		virtual int64** get_binding(const char& name)=0;
 		virtual int64* get_output(const char& name)=0;
 		virtual LGNode* clone()=0;
@@ -91,21 +116,21 @@ namespace ds_lg {
 		std::string get_type() const {return type;}
 		virtual bool has_state() {return false;}
 	protected:
+		int64 o;
 		bool endpoint;
 		std::string type;
+		monitor_container monitors;
 	};
 
 	class Input : public LGNode {
 	protected:
 		int64 *a;
-		int64 o;
 		int64 shadow;
 		std::string name;
 	public:
 		void set_name(const std::string& n){name = n;}
 		std::string get_name() const {return name;}
-		virtual void sim() { o = *a;}
-		virtual void propagate() { sim(); }
+		virtual int64 sim() { shadow = o; o = *a; return o;}
 		Input():LGNode("input"){};
 		virtual LGNode* clone() { return new Input();}
 		virtual int64* get_output(const char& name) {if (name=='o')return &o;return 0;}
@@ -126,12 +151,10 @@ namespace ds_lg {
 	class LGNode1I : public LGNode {
 	protected:
 		int64 *a;
-		int64 o;
 		int64 shadow;
 		int64 (*A)(int64_cpc a);
 	public:
-		virtual void sim() { o = (*A)(a);}
-		virtual void propagate() { sim(); }
+		virtual int64 sim() { shadow = o; ; o = (*A)(a); return o;}
 		LGNode1I(const std::string& t, int64 (*AA)(int64_cpc)):LGNode(t), A(AA){};
 		virtual LGNode* clone() { return new LGNode1I(type, A);}
 		virtual int64* get_output(const char& name) {if (name=='o')return &o; return 0;}
@@ -142,12 +165,10 @@ namespace ds_lg {
 	protected:
 		int64 * a;
 		int64 * b;
-		int64 o;
 		int64 shadow;
 		int64 (*A)(int64_cpc a, int64_cpc b);
 	public:
-		virtual void sim() { o = (*A)(a,b);}
-		virtual void propagate() { sim(); }
+		virtual int64 sim() { shadow = o; o = (*A)(a,b); return o;}
 		LGNode2I(const std::string& t, int64 (*AA)(int64_cpc, int64_cpc)):LGNode(t), A(AA){};
 		virtual LGNode* clone() { return new LGNode2I(type, A); }
 		virtual int64* get_output(const char& name) {if (name=='o')return &o; return 0;}
@@ -159,12 +180,10 @@ namespace ds_lg {
 		int64 * a;
 		int64 * b;
 		int64 * c;
-		int64 o;
 		int64 shadow;
 		int64 (*A)(int64_cpc a, int64_cpc b, int64_cpc c);
 	public:
-		virtual void sim() { o = (*A)(a,b,c);}
-		virtual void propagate() { sim(); }
+		virtual int64 sim() { shadow = o; o = (*A)(a,b,c); return o;}
 		LGNode3I(const std::string& t, int64 (*AA)(int64_cpc, int64_cpc, int64_cpc c)):LGNode(t), A(AA){};
 		virtual LGNode* clone() { return new LGNode3I(type, A); }
 		virtual int64** get_binding(const char& name);
@@ -177,12 +196,10 @@ namespace ds_lg {
 		int64 * b;
 		int64 * c;
 		int64 * d;
-		int64 o;
 		int64 shadow;
 		int64 (*A)(int64_cpc a, int64_cpc b, int64_cpc c, int64_cpc d);
 	public:
-		virtual void sim() { o = (*A)(a,b,c,d);}
-		virtual void propagate() { sim(); }
+		virtual int64 sim() { shadow = o; o = (*A)(a,b,c,d); return o;}
 		LGNode4I(const std::string& t, int64 (*AA)(int64_cpc, int64_cpc, int64_cpc, int64_cpc)):LGNode(t), A(AA){};
 		virtual LGNode* clone() { return new LGNode4I(type, A); }
 		virtual int64* get_output(const char& name) {if (name=='o')return &o; return 0;}
@@ -196,12 +213,10 @@ namespace ds_lg {
 		int64 * c;
 		int64 * d;
 		int64 * e;
-		int64 o;
 		int64 shadow;
 		int64 (*A)(int64_cpc a, int64_cpc b, int64_cpc c, int64_cpc d, int64_cpc e);
 	public:
-		virtual void sim() { o = (*A)(a,b,c,d,e);}
-		virtual void propagate() { sim(); }
+		virtual int64 sim() { shadow = o; o = (*A)(a,b,c,d,e); return o;}
 		LGNode5I(const std::string& t, int64 (*AA)(int64_cpc, int64_cpc, int64_cpc, int64_cpc, int64_cpc)):LGNode(t), A(AA){};
 		virtual LGNode* clone() { return new LGNode5I(type, A); }
 		virtual int64* get_output(const char& name) {if (name=='o')return &o; return 0;}
@@ -216,12 +231,10 @@ namespace ds_lg {
 		int64 * d;
 		int64 * e;
 		int64 * f;
-		int64 o;
 		int64 shadow;
 		int64 (*A)(int64_cpc a, int64_cpc b, int64_cpc c, int64_cpc d, int64_cpc e, int64_cpc f);
 	public:
-		virtual void sim() { o = (*A)(a,b,c,d,e,f);}
-		virtual void propagate() { sim(); }
+		virtual int64 sim() { shadow = o; o = (*A)(a,b,c,d,e,f);return o;}
 		LGNode6I(const std::string& t, int64 (*AA)(int64_cpc, int64_cpc, int64_cpc, int64_cpc, int64_cpc, int64_cpc)):LGNode(t), A(AA){};
 		virtual LGNode* clone() { return new LGNode6I(type, A); }
 		virtual int64* get_output(const char& name) {if (name=='o')return &o; return 0;}
@@ -237,12 +250,10 @@ namespace ds_lg {
 		int64 * e;
 		int64 * f;
 		int64 * g;
-		int64 o;
 		int64 shadow;
 		int64 (*A)(int64_cpc a, int64_cpc b, int64_cpc c, int64_cpc d, int64_cpc e, int64_cpc f, int64_cpc g);
 	public:
-		virtual void sim() { o = (*A)(a,b,c,d,e,f,g);}
-		virtual void propagate() { sim(); }
+		virtual int64 sim() { shadow = o; o = (*A)(a,b,c,d,e,f,g); return o;}
 		LGNode7I(const std::string& t, int64 (*AA)(int64_cpc, int64_cpc, int64_cpc, int64_cpc, int64_cpc, int64_cpc, int64_cpc)):LGNode(t), A(AA){};
 		virtual LGNode* clone() { return new LGNode7I(type, A); }
 		virtual int64* get_output(const char& name) {if (name=='o')return &o; return 0;}
@@ -259,12 +270,10 @@ namespace ds_lg {
 		int64 * f;
 		int64 * g;
 		int64 * h;
-		int64 o;
 		int64 shadow;
 		int64 (*A)(int64_cpc a, int64_cpc b, int64_cpc c, int64_cpc d, int64_cpc e, int64_cpc f, int64_cpc g, int64_cpc h);
 	public:
-		virtual void sim() { o = (*A)(a,b,c,d,e,f,g,h);}
-		virtual void propagate() { sim(); }
+		virtual int64 sim() { shadow = o; o = (*A)(a,b,c,d,e,f,g,h);return o;}
 		LGNode8I(const std::string& t, int64 (*AA)(int64_cpc, int64_cpc, int64_cpc, int64_cpc, int64_cpc, int64_cpc, int64_cpc, int64_cpc)):LGNode(t), A(AA){};
 		virtual LGNode* clone() { return new LGNode8I(type, A); }
 		virtual int64* get_output(const char& name) {if (name=='o')return &o; return 0;}
@@ -279,13 +288,13 @@ namespace ds_lg {
 		int64_cpc op;
 		int64 ** input_array;
 	public:
-		virtual void sim() {
+		virtual int64 sim() {
 			o = **input_array;
 			for (int i=0;i<inputSize;i++){
 				o = (*A)(op, input_array[i]);
 			}
+			return o;
 		}
-		virtual void propagate() { sim(); }
 		LGNodeArr(const std::string& t, const int& inputs, int64 (*AA)(int64_cpc, int64_cpc)):LGNode(t), inputSize(inputs), A(AA), op(&o){
 			input_array = new int64*[inputSize];
 		};
@@ -326,6 +335,7 @@ namespace ds_lg {
 		lg_container::iterator get_outputs_begin(){return outputs.begin();}
 		lg_container::iterator get_outputs_end(){return outputs.end();}
 	};
+
 }
 
 #endif /* LG_H_ */
--- ds/test/lib_test.cpp
+++ ds/test/lib_test.cpp
@@ -5,296 +5,278 @@
  *      Author: cookao
  */
 #define BOOST_TEST_DYN_LINK
-#define BOOST_TEST_MODULE MyTest
+#define BOOST_TEST_MODULE LibTest
 #include <boost/test/unit_test.hpp>
 #include "ds_library.h"
 #include <iostream>
 #include <boost/spirit/include/qi.hpp>
 #include "ds_workspace.h"
 
-	BOOST_AUTO_TEST_CASE( lib_test_open ) {
-		using namespace ds_library;
-		LibraryFactory *factory = LibraryFactory::getInstance();
-		Library *defaultLib = factory->loadLibrary();
-		BOOST_REQUIRE(defaultLib !=0);
-		factory->remove_library();
+BOOST_AUTO_TEST_CASE( lib_test_open ) {
+	using namespace ds_library;
+	LibraryFactory *factory = LibraryFactory::getInstance();
+	Library *defaultLib = factory->loadLibrary();
+	BOOST_REQUIRE(defaultLib !=0);
+	factory->remove_library();
 
-		std::string path_inconsistent = "default_lib_inconsistent";
-		Library *inconsistentLib = factory->loadLibrary(path_inconsistent);
-		BOOST_REQUIRE(inconsistentLib ==0);
-		factory->remove_library(path_inconsistent);
-
-		std::string path_unknown = "default_lib_unknown";
-		Library *unknownLib = factory->loadLibrary(path_unknown);
-		BOOST_REQUIRE(unknownLib ==0);
+	std::string path_inconsistent = "default_lib_inconsistent";
+	Library *inconsistentLib = factory->loadLibrary(path_inconsistent);
+	BOOST_REQUIRE(inconsistentLib ==0);
+	factory->remove_library(path_inconsistent);
+	std::string path_unknown = "default_lib_unknown";
+	Library *unknownLib = factory->loadLibrary(path_unknown);
+	BOOST_REQUIRE(unknownLib ==0);
 		factory->remove_library(path_unknown);
 	}
 
-	BOOST_AUTO_TEST_CASE( lib_test_parse ) {
-		using namespace ds_library;
-		std::string t("and8,and8,1,o1@o,8,i1@a,i2@b,i3@c,i4@d,i5@e,i6@f,i7@g,i8@h");
-		parse_lib_node n;
-		lib_parser<std::string::iterator> p;
-		lib_skipper<std::string::iterator> skipper;
-		bool parse =  boost::spirit::qi::phrase_parse(t.begin(), t.end(), p, skipper, n);
-		BOOST_CHECK(parse);
-		t = "and8,and8,1,o1@o,8,i1@a,i2@b,i3@c,i4@d,i5@e,i6@f,i7@g,i8@h# this is a comment";
-		parse =  boost::spirit::qi::phrase_parse(t.begin(), t.end(), p, skipper, n);
-		BOOST_CHECK(parse);
-		BOOST_CHECK(!n.flexible);
-		t = "and8,and8,1,o1@o,8,i1@a,  i2@b, i3@c,i4@d, i5 @e,i6@ f,i7@g,i8@h";
-		parse =  boost::spirit::qi::phrase_parse(t.begin(), t.end(), p, skipper, n);
-		BOOST_CHECK(parse);
-		t = "and8,and8,1,o1@o,8,i1@1,i2@b,i3@c,i4@d,i5 @e,i6@f,i7@g,i8@h";
-		parse =  boost::spirit::qi::phrase_parse(t.begin(), t.end(), p, skipper, n);
-		BOOST_CHECK(!parse);
-		t = "and8,and8,1,o1@o,i1@a,i2@b,i3@c,i4@d,i5 @e,i6@f,i7@g,i8@h";
-		parse =  boost::spirit::qi::phrase_parse(t.begin(), t.end(), p, skipper, n);
-		BOOST_CHECK(!parse);
-		t = "and8*,and8,1,o1@o,8,i1@a,  i2@b, i3@c,i4@d, i5 @e,i6@ f,i7@g,i8@h";
-		parse =  boost::spirit::qi::phrase_parse(t.begin(), t.end(), p, skipper, n);
-		BOOST_CHECK(parse);
-		BOOST_CHECK(n.flexible);
-	}
+BOOST_AUTO_TEST_CASE( lib_test_parse ) {
+	using namespace ds_library;
+	std::string t("and8,and8,1,o1@o,8,i1@a,i2@b,i3@c,i4@d,i5@e,i6@f,i7@g,i8@h");
+	parse_lib_node n;
+	lib_parser<std::string::iterator> p;
+	lib_skipper<std::string::iterator> skipper;
+	bool parse =  boost::spirit::qi::phrase_parse(t.begin(), t.end(), p, skipper, n);
+	BOOST_CHECK(parse);
+	t = "and8,and8,1,o1@o,8,i1@a,i2@b,i3@c,i4@d,i5@e,i6@f,i7@g,i8@h# this is a comment";
+	parse =  boost::spirit::qi::phrase_parse(t.begin(), t.end(), p, skipper, n);
+	BOOST_CHECK(parse);
+	BOOST_CHECK(!n.flexible);
+	t = "and8,and8,1,o1@o,8,i1@a,  i2@b, i3@c,i4@d, i5 @e,i6@ f,i7@g,i8@h";
+	parse =  boost::spirit::qi::phrase_parse(t.begin(), t.end(), p, skipper, n);
+	BOOST_CHECK(parse);
+	t = "and8,and8,1,o1@o,8,i1@1,i2@b,i3@c,i4@d,i5 @e,i6@f,i7@g,i8@h";
+	parse =  boost::spirit::qi::phrase_parse(t.begin(), t.end(), p, skipper, n);
+	BOOST_CHECK(!parse);
+	t = "and8,and8,1,o1@o,i1@a,i2@b,i3@c,i4@d,i5 @e,i6@f,i7@g,i8@h";
+	parse =  boost::spirit::qi::phrase_parse(t.begin(), t.end(), p, skipper, n);
+	BOOST_CHECK(!parse);
+	t = "and8*,and8,1,o1@o,8,i1@a,  i2@b, i3@c,i4@d, i5 @e,i6@ f,i7@g,i8@h";
+	parse =  boost::spirit::qi::phrase_parse(t.begin(), t.end(), p, skipper, n);
+	BOOST_CHECK(parse);
+	BOOST_CHECK(n.flexible);
+}
 
-	BOOST_AUTO_TEST_CASE( lib_gate_query ) {
-		using namespace ds_library;
-		using namespace ds_structural;
-		LibraryFactory *factory = LibraryFactory::getInstance();
-		Library *defaultLib = factory->loadLibrary();
-
+BOOST_AUTO_TEST_CASE( lib_gate_query ) {
+	using namespace ds_library;
+	using namespace ds_structural;
+	LibraryFactory *factory = LibraryFactory::getInstance();
+	Library *defaultLib = factory->loadLibrary();
 		Gate* g = defaultLib->getGate("and4",5);
-		BOOST_REQUIRE(g!=0);
-		BOOST_REQUIRE(g->get_type()=="and4");
-		BOOST_REQUIRE(g->get_lgn()!=0);
-		BOOST_REQUIRE(g->get_num_ports()==5);
-		std::size_t inputs = 0;
-		typedef port_container::const_iterator IT;
-		for (IT it=g->get_inputs()->begin();it!=g->get_inputs()->end();it++){
-			std::string name = (*it)->get_instance_name();
-			BOOST_REQUIRE(!(g->get_mapping(name)).empty());
-			inputs++;
-		}
-		std::size_t outputs = 0;
-		typedef port_container::const_iterator IT;
-		for (IT it=g->get_outputs()->begin();it!=g->get_outputs()->end();it++){
-			std::string name = (*it)->get_instance_name();
-			BOOST_REQUIRE(!(g->get_mapping(name)).empty());
-			outputs++;
-		}
-		BOOST_REQUIRE(g->get_num_ports() == inputs + outputs);
-
-		delete(g);
-		g = defaultLib->getGate("or",7);
-		BOOST_REQUIRE(g!=0);
-		BOOST_REQUIRE(g->get_type()=="or");
-		BOOST_REQUIRE(g->get_lgn()!=0);
-		BOOST_REQUIRE(g->get_num_ports()==7);
-		BOOST_REQUIRE(g->get_inputs()->size()==6);
-		BOOST_REQUIRE(g->get_outputs()->size()==1);
-		inputs = 0;
-		typedef port_container::const_iterator IT;
-		for (IT it=g->get_inputs()->begin();it!=g->get_inputs()->end();it++){
-			std::string name = (*it)->get_instance_name();
-			BOOST_REQUIRE(!(g->get_mapping(name)).empty());
-			inputs++;
-		}
-		outputs = 0;
-		typedef port_container::const_iterator IT;
-		for (IT it=g->get_outputs()->begin();it!=g->get_outputs()->end();it++){
-			std::string name = (*it)->get_instance_name();
-			BOOST_REQUIRE(!(g->get_mapping(name)).empty());
-			outputs++;
-		}
-		BOOST_REQUIRE(g->get_num_ports() == inputs + outputs);
-		delete(g);
+	BOOST_REQUIRE(g!=0);
+	BOOST_REQUIRE(g->get_type()=="and4");
+	BOOST_REQUIRE(g->get_lgn()!=0);
+	BOOST_REQUIRE(g->get_num_ports()==5);
+	std::size_t inputs = 0;
+	typedef port_container::const_iterator IT;
+	for (IT it=g->get_inputs()->begin();it!=g->get_inputs()->end();it++){
+		std::string name = (*it)->get_instance_name();
+		BOOST_REQUIRE(!(g->get_mapping(name)).empty());
+		inputs++;
 	}
-
-	BOOST_AUTO_TEST_CASE(netlist_import) {
-
-		using namespace ds_library;
-		using namespace ds_structural;
-		using namespace ds_workspace;
-
-		LibraryFactory *factory = LibraryFactory::getInstance();
-		Library *defaultLib = factory->loadLibrary();
-		const char* d = getenv("DS");
-		std::string path = d?d:"";
-
-		Workspace *wp = ds_workspace::Workspace::get_workspace();
-		wp->add_library(defaultLib);
-
-		try {
-
-			std::string file = path + "/files/p45k.v";
-			std::cout << "... importing " << file << std::endl;
-			ds_structural::NetList *nl = ds_library::import(file, "top", wp);
-			BOOST_CHECK(nl!=0);
-			BOOST_ASSERT(nl->check_netlist());
-			delete nl;
-
-			file = path + "/files/p100k.v";
-			std::cout << "... importing " << file << std::endl;
-			nl = ds_library::import(file, "top", wp);
-			BOOST_CHECK(nl!=0);
-			BOOST_ASSERT(nl->check_netlist());
-			delete nl;
-
-			file = path + "/files/p141k.v";
-			std::cout << "... importing " << file << std::endl;
-			nl = ds_library::import(file, "top", wp);
-			BOOST_CHECK(nl!=0);
-			BOOST_ASSERT(nl->check_netlist());
-			delete nl;
-
-			file = path + "/files/p239k.v";
-			std::cout << "... importing " << file << std::endl;
-			nl = ds_library::import(file, "p239k", wp);
-			BOOST_CHECK(nl!=0);
-			BOOST_ASSERT(nl->check_netlist());
-			delete nl;
-
-			file = path + "/files/p259k.v";
-			std::cout << "... importing " << file << std::endl;
-			nl = ds_library::import(file, "p259k", wp);
-			BOOST_CHECK(nl!=0);
-			BOOST_ASSERT(nl->check_netlist());
-			delete nl;
-
-			file = path + "/files/p267k.v";
-			std::cout << "... importing " << file << std::endl;
-			nl = ds_library::import(file, "top", wp);
-			BOOST_CHECK(nl!=0);
-			BOOST_ASSERT(nl->check_netlist());
-			delete nl;
-
-			file = path + "/files/p269k.v";
-			std::cout << "... importing " << file << std::endl;
-			nl = ds_library::import(file, "top", wp);
-			BOOST_CHECK(nl!=0);
-			BOOST_ASSERT(nl->check_netlist());
-			delete nl;
-
-			file = path + "/files/p279k.v";
-			std::cout << "... importing " << file << std::endl;
-			nl = ds_library::import(file, "top", wp);
-			BOOST_CHECK(nl!=0);
-			BOOST_ASSERT(nl->check_netlist());
-			delete nl;
-
-			file = path + "/files/p286k.v";
-			std::cout << "... importing " << file << std::endl;
-			nl = ds_library::import(file, "top", wp);
-			BOOST_CHECK(nl!=0);
-			BOOST_ASSERT(nl->check_netlist());
-			delete nl;
-
-			file = path + "/files/p295k.v";
-			std::cout << "... importing " << file << std::endl;
-			nl = ds_library::import(file, "top", wp);
-			BOOST_CHECK(nl!=0);
-			BOOST_ASSERT(nl->check_netlist());
-			delete nl;
-
-			file = path + "/files/p330k.v";
-			std::cout << "... importing " << file << std::endl;
-			nl = ds_library::import(file, "top", wp);
-			BOOST_CHECK(nl!=0);
-			BOOST_ASSERT(nl->check_netlist());
-			delete nl;
-
-		} catch (boost::exception& e){
-			 if( std::string *mi = boost::get_error_info<ds_common::errmsg_info>(e) )
-				 std::cerr << "Error: " << *mi;
-		}
+	std::size_t outputs = 0;
+	typedef port_container::const_iterator IT;
+	for (IT it=g->get_outputs()->begin();it!=g->get_outputs()->end();it++){
+		std::string name = (*it)->get_instance_name();
+		BOOST_REQUIRE(!(g->get_mapping(name)).empty());
+		outputs++;
 	}
+	BOOST_REQUIRE(g->get_num_ports() == inputs + outputs);
+	delete(g);
+	g = defaultLib->getGate("or",7);
+	BOOST_REQUIRE(g!=0);
+	BOOST_REQUIRE(g->get_type()=="or");
+	BOOST_REQUIRE(g->get_lgn()!=0);
+	BOOST_REQUIRE(g->get_num_ports()==7);
+	BOOST_REQUIRE(g->get_inputs()->size()==6);
+	BOOST_REQUIRE(g->get_outputs()->size()==1);
+	inputs = 0;
+	typedef port_container::const_iterator IT;
+	for (IT it=g->get_inputs()->begin();it!=g->get_inputs()->end();it++){
+		std::string name = (*it)->get_instance_name();
+		BOOST_REQUIRE(!(g->get_mapping(name)).empty());
+		inputs++;
+	}
+	outputs = 0;
+	typedef port_container::const_iterator IT;
+	for (IT it=g->get_outputs()->begin();it!=g->get_outputs()->end();it++){
+		std::string name = (*it)->get_instance_name();
+		BOOST_REQUIRE(!(g->get_mapping(name)).empty());
+		outputs++;
+	}
+	BOOST_REQUIRE(g->get_num_ports() == inputs + outputs);
+	delete(g);
+}
 
-	BOOST_AUTO_TEST_CASE(verilog_parser) {
-		using namespace ds_library;
+BOOST_AUTO_TEST_CASE(netlist_import) {
 
-		const char* d = getenv("DS");
-		std::string path = d?d:"";
-		std::vector<ds_library::parse_netlist> netlists;
-		bool parse = false;
-		std::string file;
-
-		file = path + "/files/p45k.v";
-		std::cout << "...parsing " << file << std::endl;
-		parse = parse_verilog(file, netlists);
-		BOOST_CHECK(parse);
-		BOOST_CHECK(netlists.size() == 2);
-		netlists.clear();
-
+	using namespace ds_library;
+	using namespace ds_structural;
+	using namespace ds_workspace;
+	LibraryFactory *factory = LibraryFactory::getInstance();
+	Library *defaultLib = factory->loadLibrary();
+	const char* d = getenv("DS");
+	std::string path = d?d:"";
+	Workspace *wp = ds_workspace::Workspace::get_workspace();
+	wp->add_library(defaultLib);
+	try {
+		std::string file = path + "/files/p45k.v";
+		std::cout << "... importing " << file << std::endl;
+		ds_structural::NetList *nl = ds_library::import(file, "top", wp);
+		BOOST_CHECK(nl!=0);
+		BOOST_ASSERT(nl->check_netlist());
+		delete nl;
 		file = path + "/files/p100k.v";
-		std::cout << "... parsing " << file << std::endl;
-		parse = parse_verilog(file, netlists);
-		BOOST_CHECK(parse);
-		BOOST_CHECK(netlists.size()==2);
-		netlists.clear();
+		std::cout << "... importing " << file << std::endl;
+		nl = ds_library::import(file, "top", wp);
+		BOOST_CHECK(nl!=0);
+		BOOST_ASSERT(nl->check_netlist());
+		delete nl;
 
 		file = path + "/files/p141k.v";
-		std::cout << "... parsing " << file << std::endl;
-		parse = parse_verilog(file, netlists);
-		BOOST_CHECK(parse);
-		BOOST_CHECK(netlists.size()==3);
-		netlists.clear();
+		std::cout << "... importing " << file << std::endl;
+		nl = ds_library::import(file, "top", wp);
+		BOOST_CHECK(nl!=0);
+		BOOST_ASSERT(nl->check_netlist());
+		delete nl;
 
 		file = path + "/files/p239k.v";
-		std::cout << "... parsing " << file << std::endl;
-		parse = parse_verilog(file, netlists);
-		BOOST_CHECK(parse);
-		BOOST_CHECK(netlists.size()==1);
-		netlists.clear();
-
+		std::cout << "... importing " << file << std::endl;
+		nl = ds_library::import(file, "p239k", wp);
+		BOOST_CHECK(nl!=0);
+		BOOST_ASSERT(nl->check_netlist());
+		delete nl;
 		file = path + "/files/p259k.v";
-		std::cout << "... parsing " << file << std::endl;
-		parse = parse_verilog(file, netlists);
-		BOOST_CHECK(parse);
-		BOOST_CHECK(netlists.size()>0);
-		netlists.clear();
-
+		std::cout << "... importing " << file << std::endl;
+		nl = ds_library::import(file, "p259k", wp);
+		BOOST_CHECK(nl!=0);
+		BOOST_ASSERT(nl->check_netlist());
+		delete nl;
 		file = path + "/files/p267k.v";
-		std::cout << "... parsing " << file << std::endl;
-		parse = parse_verilog(file, netlists);
-		BOOST_CHECK(parse);
-		BOOST_CHECK(netlists.size()>0);
-		netlists.clear();
-
+		std::cout << "... importing " << file << std::endl;
+		nl = ds_library::import(file, "top", wp);
+		BOOST_CHECK(nl!=0);
+		BOOST_ASSERT(nl->check_netlist());
+		delete nl;
 		file = path + "/files/p269k.v";
-		std::cout << "... parsing " << file << std::endl;
-		parse = parse_verilog(file, netlists);
-		BOOST_CHECK(parse);
-		BOOST_CHECK(netlists.size()>0);
-		netlists.clear();
+		std::cout << "... importing " << file << std::endl;
+		nl = ds_library::import(file, "top", wp);
+		BOOST_CHECK(nl!=0);
+		BOOST_ASSERT(nl->check_netlist());
+		delete nl;
 
 		file = path + "/files/p279k.v";
-		std::cout << "... parsing " << file << std::endl;
-		parse = parse_verilog(file, netlists);
-		BOOST_CHECK(parse);
-		BOOST_CHECK(netlists.size()>0);
-		netlists.clear();
-
+		std::cout << "... importing " << file << std::endl;
+		nl = ds_library::import(file, "p279k", wp);
+		BOOST_CHECK(nl!=0);
+		BOOST_ASSERT(nl->check_netlist());
+		delete nl;
 		file = path + "/files/p286k.v";
-		std::cout << "... parsing " << file << std::endl;
-		parse = parse_verilog(file, netlists);
-		BOOST_CHECK(parse);
-		BOOST_CHECK(netlists.size()>0);
-		netlists.clear();
+		std::cout << "... importing " << file << std::endl;
+		nl = ds_library::import(file, "p286k", wp);
+		BOOST_CHECK(nl!=0);
+		BOOST_ASSERT(nl->check_netlist());
+		delete nl;
 
 		file = path + "/files/p295k.v";
-		std::cout << "... parsing " << file << std::endl;
-		parse = parse_verilog(file, netlists);
-		BOOST_CHECK(parse);
-		BOOST_CHECK(netlists.size()>0);
-		netlists.clear();
-
+		std::cout << "... importing " << file << std::endl;
+		nl = ds_library::import(file, "top", wp);
+		BOOST_CHECK(nl!=0);
+		BOOST_ASSERT(nl->check_netlist());
+		delete nl;
 		file = path + "/files/p330k.v";
-		std::cout << "... parsing " << file << std::endl;
-		parse = parse_verilog(file, netlists);
-		BOOST_CHECK(parse);
-		BOOST_CHECK(netlists.size()>0);
-		netlists.clear();
+		std::cout << "... importing " << file << std::endl;
+		nl = ds_library::import(file, "top", wp);
+		BOOST_CHECK(nl!=0);
+		BOOST_ASSERT(nl->check_netlist());
+		delete nl;
+	} catch (boost::exception& e){
+	 if( std::string *mi = boost::get_error_info<ds_common::errmsg_info>(e) )
+		 std::cerr << "Error: " << *mi;
 	}
+}
+
+BOOST_AUTO_TEST_CASE(verilog_parser) {
+	using namespace ds_library;
+	const char* d = getenv("DS");
+	std::string path = d?d:"";
+	std::vector<ds_library::parse_netlist> netlists;
+	bool parse = false;
+	std::string file;
+
+	file = path + "/files/p45k.v";
+	std::cout << "...parsing " << file << std::endl;
+	parse = parse_verilog(file, netlists);
+	BOOST_CHECK(parse);
+	BOOST_CHECK(netlists.size() == 2);
+	netlists.clear();
+	file = path + "/files/p100k.v";
+	std::cout << "... parsing " << file << std::endl;
+	parse = parse_verilog(file, netlists);
+	BOOST_CHECK(parse);
+	BOOST_CHECK(netlists.size()==2);
+	netlists.clear();
+
+	file = path + "/files/p141k.v";
+	std::cout << "... parsing " << file << std::endl;
+	parse = parse_verilog(file, netlists);
+	BOOST_CHECK(parse);
+	BOOST_CHECK(netlists.size()==3);
+	netlists.clear();
+
+	file = path + "/files/p239k.v";
+	std::cout << "... parsing " << file << std::endl;
+	parse = parse_verilog(file, netlists);
+	BOOST_CHECK(parse);
+	BOOST_CHECK(netlists.size()==1);
+	netlists.clear();
+
+	file = path + "/files/p259k.v";
+	std::cout << "... parsing " << file << std::endl;
+	parse = parse_verilog(file, netlists);
+	BOOST_CHECK(parse);
+	BOOST_CHECK(netlists.size()>0);
+	netlists.clear();
+
+	file = path + "/files/p267k.v";
+	std::cout << "... parsing " << file << std::endl;
+	parse = parse_verilog(file, netlists);
+	BOOST_CHECK(parse);
+	BOOST_CHECK(netlists.size()>0);
+	netlists.clear();
+	file = path + "/files/p269k.v";
+	std::cout << "... parsing " << file << std::endl;
+	parse = parse_verilog(file, netlists);
+	BOOST_CHECK(parse);
+	BOOST_CHECK(netlists.size()>0);
+	netlists.clear();
+
+	file = path + "/files/p279k.v";
+	std::cout << "... parsing " << file << std::endl;
+	parse = parse_verilog(file, netlists);
+	BOOST_CHECK(parse);
+	BOOST_CHECK(netlists.size()>0);
+	netlists.clear();
+
+	file = path + "/files/p286k.v";
+	std::cout << "... parsing " << file << std::endl;
+	parse = parse_verilog(file, netlists);
+	BOOST_CHECK(parse);
+	BOOST_CHECK(netlists.size()>0);
+	netlists.clear();
+	file = path + "/files/p295k.v";
+	std::cout << "... parsing " << file << std::endl;
+	parse = parse_verilog(file, netlists);
+	BOOST_CHECK(parse);
+	BOOST_CHECK(netlists.size()>0);
+	netlists.clear();
+
+	file = path + "/files/p330k.v";
+	std::cout << "... parsing " << file << std::endl;
+	parse = parse_verilog(file, netlists);
+	BOOST_CHECK(parse);
+	BOOST_CHECK(netlists.size()>0);
+	netlists.clear();
+}
 
 
 
